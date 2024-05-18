4552659afdd2c778df62b6c5c23c57151873498b
Vitor Guedes <126086115+VitorGuedes22@users.noreply.github.com>
2024-05-18 17:32:33 -0300

    first commit
    seção 4.4 do livro finalizada sem erros e renderização ok no navegador

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,38 @@
+class MoviesController < ApplicationController
+  # Outros métodos do controlador
+
+  def create
+    @movie = Movie.new(movie_params)
+    if @movie.save
+      redirect_to @movie, notice: 'Movie was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def update
+    @movie = Movie.find(params[:id])
+    if @movie.update(movie_params)
+      redirect_to @movie, notice: 'Movie was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def index
+    @movies = Movie.all
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:title, :rating, :description, :release_date)
+  end
+
+  def show
+    id = params[:id] # retrieve movie ID from URI route
+    @movie = Movie.find(id) # look up movie by unique ID
+    # will render app/views/movies/show.html.haml by default
+  end
+
+end
